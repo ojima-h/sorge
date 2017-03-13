1b68db05de881292104b5e356e8ec6a33daa3d5d
Hikaru Ojima <ojima.h@gmail.com>
2017-03-13 16:14:51 +0900

    Fix test.

--- test/sorge/model/event_queue_test.rb
+++ test/sorge/model/event_queue_test.rb
@@ -9,11 +9,15 @@ module Sorge
 
       def test_ok
         queue.post('foo', bar: 1)
+        queue.post('foo', bar: 2)
+        queue.post('foo', bar: 3)
 
         msg = queue.get
-
         assert_equal 'foo', msg.name
         assert_equal({ bar: 1 }, msg.data)
+
+        msg = queue.get
+        assert_equal({ bar: 2 }, msg.data)
       end
     end
   end
